f111341aaf6d884f5da02f216325181ff2b54444
Daniel Caldas <89937551+DaniCaldas@users.noreply.github.com>
2024-07-15 13:56:49 -0300

    Update ListController.java

--- src/main/java/com/example/demo/Controller/ListController.java
+++ src/main/java/com/example/demo/Controller/ListController.java
@@ -3,7 +3,7 @@ package com.example.demo.Controller;
 import com.example.demo.Lista.*;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-import org.hibernate.mapping.List;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
